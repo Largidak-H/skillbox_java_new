af8d1785b0a17a5f338891758e0536864827564b
Largidak-H <largidak-h@yandex.ru>
2020-06-18 00:02:00 +0500

    maps_#2

--- 05_ArraysAndCollections/4/Phonebook/src/Loader.java
+++ 05_ArraysAndCollections/4/Phonebook/src/Loader.java
@@ -5,20 +5,18 @@ import java.util.regex.Pattern;
 
 public class Loader {
     public static void main(String[] args) {
+        final String regular = "[А-ЯЁа-яё&&[^ЪЬЫъыь]]+\\s[А-ЯЁа-яё&&[^ЪЬЫъыь]]+\\s[А-ЯЁа-яё&&[^ЪЬЫъыь]]+";
         TreeMap<String, String> phoneBook = new TreeMap<>();
         Scanner reader = new Scanner(System.in);
         while (true) {
             System.out.println("Введите команду \"LIST\", Ф.И.О. абонента или номер телефона абонента:");
             String command = reader.nextLine().trim();
             if (command.equalsIgnoreCase("LIST")) {
-                if (!phoneBook.isEmpty()) {
-                    printMap(phoneBook);
-                } else System.out.println("Телефонная книга пуста.");
+                printMap(phoneBook);
             } else {
-                if (Pattern.compile("^[А-ЯЁа-яё&&[^ЪЬЫъыь]]+\\s[А-ЯЁа-яё&&[^ЪЬЫъыь]]+\\s[А-ЯЁа-яё&&[^ЪЬЫъыь]]+$")
-                        .matcher(command).matches()) {
+                if (Pattern.compile(regular).matcher(command).matches()) {
                     if (phoneBook.containsValue(command)) {
-                        for (Map.Entry entry : phoneBook.entrySet()) {
+                        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
                             if (entry.getValue().equals(command)) {
                                 System.out.println(command + ", номер телефона: " + entry.getKey());
                             }
@@ -27,24 +25,22 @@ public class Loader {
                         System.out.println("Абонента с таким Ф.И.О в телефонной книге нет. " +
                                 "Пожалуйста, введите номер телефона абонента (10-11 цифр):");
                         String phoneNumber = reader.nextLine().replaceAll("[^0-9]+", "").trim();
-                        if (Pattern.compile("^[0-9]{10,11}+$").matcher(phoneNumber).matches()) {
-                            phoneNumber = checkNumber(phoneNumber);
+                        if (Pattern.compile("[0-9]{10,11}+").matcher(phoneNumber).matches()) {
+                            phoneNumber = makePrefix7(phoneNumber);
                             phoneBook.put(phoneNumber, command);
                             System.out.println("Номер телефонна введен успешно.");
                         } else
                             System.out.println("Номер телефонна введен неверно!");
                     }
-                } else if (Pattern.compile("^[0-9]{10,11}+$").matcher(command = command.replaceAll("[^0-9]+",
-                        "").trim()).matches()) {
-                    command = checkNumber(command);
+                } else if (Pattern.compile("[0-9]{10,11}+").matcher(cleanPhone(command).trim()).matches()) {
+                    command = makePrefix7(command);
                     if (phoneBook.containsKey(command)) {
                         System.out.println(phoneBook.get(command) + ", номер телефона: " + command);
                     } else {
                         System.out.println("Абонента с таким номером телефона в телефонной книге нет. " +
                                 "Пожалуйста, введите Ф.И.О. абонента:");
                         String name = reader.nextLine().trim();
-                        if (Pattern.compile("^[А-ЯЁа-яё&&[^ЪЬЫъыь]]+\\s[А-ЯЁа-яё&&[^ЪЬЫъыь]]+\\s[А-ЯЁа-яё&&[^ЪЬЫъыь]]+$")
-                                .matcher(name).matches()) {
+                        if (Pattern.compile(regular).matcher(name).matches()) {
                             phoneBook.put(command, name);
                             System.out.println("Ф.И.О. введены успешно.");
                         } else
@@ -58,16 +54,23 @@ public class Loader {
     }
 
     private static void printMap(Map<String, String> anyMap) {
-        for (String key : anyMap.keySet()) {
-            System.out.println(anyMap.get(key) + ", номер телефона: " + key);
-        }
+        if (!anyMap.isEmpty()) {
+            for (String key : anyMap.keySet()) {
+                System.out.println(anyMap.get(key) + ", номер телефона: " + key);
+            }
+        } else System.out.println("Телефонная книга пуста.");
     }
 
-    private static String checkNumber(String number) {
+    private static String makePrefix7(String number) {
         if (number.length() == 11) {
             number = number.substring(1);
         }
         number = 7 + number;
+        return number;
+    }
+
+    private static String cleanPhone(String number) {
+        number = number.replaceAll("[^0-9]+", "");
         return number;
     }
 }
