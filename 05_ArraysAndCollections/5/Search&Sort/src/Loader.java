1137d2704436c233f37075ebc439c0d5d3fc8146
Largidak-H <largidak-h@yandex.ru>
2020-07-29 20:19:38 +0500

    search_sort_#2

--- 05_ArraysAndCollections/5/Search&Sort/src/Loader.java
+++ 05_ArraysAndCollections/5/Search&Sort/src/Loader.java
@@ -4,49 +4,21 @@ import java.util.regex.Pattern;
 public class Loader {
     public static void main(String[] args) {
         char[] symbols = {'А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х'};
-        int [] digitsOfRegions = new int[200];
 
-        for (int i=1;i<digitsOfRegions.length;i++) {
-            digitsOfRegions[i]=i;
-        }
-
-        ArrayList<String> listOfNiceNumbers = new ArrayList<>();
-
-        int countOfNumbers = 1000;
-
-        for (char symbol : symbols) {
-            for (int numbers = 0; numbers < countOfNumbers; numbers++) {
-                String number = String.format("%1$c%2$03d%1$c%1$c", symbol, numbers);
-                if (!listOfNiceNumbers.contains(number)) {
-                    listOfNiceNumbers.add(number);
-                }
-            }
-        }
-
-        int countOfDigits = 10;
+       ArrayList<String> listOfNiceNumbers = new ArrayList<>();
 
         for (char symbol1 : symbols) {
             for (char symbol2 : symbols) {
                 for (char symbol3 : symbols) {
-                    for (int digits = 0; digits < countOfDigits; digits++) {
-                        String number = String.format("%2$c%1$d%1$d%1$d%3$c%4$c", digits, symbol1, symbol2, symbol3);
-                        if (!listOfNiceNumbers.contains(number))
+                    for (int digits = 111; digits <= 999; digits += 111) {
+                        for (int region = 1; region <= 200; region++) {
+                            String number = String.format("%2$c%1$d%3$c%4$c%5$02d", digits, symbol1, symbol2, symbol3, region);
                             listOfNiceNumbers.add(number);
-                        number = String.format("%2$c%1$d00%3$c%4$c", digits, symbol1, symbol2, symbol3);
-                        if (!listOfNiceNumbers.contains(number))
-                            listOfNiceNumbers.add(number);
+                        }
                     }
                 }
             }
         }
-
-        ArrayList<String> result = new ArrayList<>();
-
-        for (String number : listOfNiceNumbers)
-            for (int region : digitsOfRegions)
-            {
-                result.add(String.format("%1$s%2$02d", number, region));
-            }
 
         Scanner reader = new Scanner(System.in);
 
@@ -56,17 +28,17 @@ public class Loader {
             if (!Pattern.compile("[АВЕКМНОРСТУХ]\\d{3}[АВЕКМНОРСТУХ]{2}\\d[1-9]+$").matcher(ourNumber).matches())
                 System.out.println("Неверно введен номер");
             else {
-                HashSet<String> resultHash = new HashSet<>(result);
-                TreeSet<String> resultTree = new TreeSet<>(result);
+                HashSet<String> resultHash = new HashSet<>(listOfNiceNumbers);
+                TreeSet<String> resultTree = new TreeSet<>(listOfNiceNumbers);
 
                 long startTime = System.nanoTime();
-                boolean blat = result.contains(ourNumber);
+                boolean blat = listOfNiceNumbers.contains(ourNumber);
                 long finTime = System.nanoTime();
                 System.out.println("Поиск перебором: " + checkBlat(blat) + "поиск занял " + (finTime - startTime) + "нс");
 
-                Collections.sort(result);
+                Collections.sort(listOfNiceNumbers);
                 long startTimeBin = System.nanoTime();
-                int a = Collections.binarySearch(result,ourNumber);
+                int a = Collections.binarySearch(listOfNiceNumbers,ourNumber);
                 long finTimeBin = System.nanoTime();
                 System.out.println("Бинарный поиск: " + checkBlat(a) + "поиск занял " + (finTimeBin - startTimeBin) + "нс");
 
@@ -88,6 +60,6 @@ public class Loader {
     }
 
     private static String checkBlat(int b) {
-        return b > 0 ? "номер найден, " : "номер не найден, ";
+        return b >= 0 ? "номер найден, " : "номер не найден, ";
     }
 }
