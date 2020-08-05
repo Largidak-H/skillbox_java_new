72e80b8401cdacda2c38ed1a6b55e1413e561c1d
Largidak-H <largidak-h@yandex.ru>
2020-08-05 15:48:09 +0500

    Airport_#2

--- 07_AdvancedOOPFeatures/2/Airport/src/Loader.java
+++ 07_AdvancedOOPFeatures/2/Airport/src/Loader.java
@@ -1,17 +1,14 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import com.skillbox.airport.Terminal;
 
 import java.util.*;
 
 public class Loader {
     public static void main(String[] args) {
         Airport airport = Airport.getInstance();
-        List<Terminal> terminalList = airport.getTerminals();
-        List<Flight> flightList = new ArrayList<>();
-        terminalList.forEach(t -> flightList.addAll(t.getFlights()));
-        flightList.stream()
-                .filter(f -> f.getType().equals(Flight.Type.ARRIVAL))
+        airport.getTerminals().stream()
+                .flatMap(t -> t.getFlights().stream())
+                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
                 .filter(f -> {
                     Calendar calendar = new GregorianCalendar();
                     calendar.add(Calendar.HOUR_OF_DAY, 2);
