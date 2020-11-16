240c912b40c0dc7c421ce8d3c7f3910a77a77899
Largidak-H <largidak-h@yandex.ru>
2020-11-16 23:33:16 +0500

    FileReading_#2

--- 09_FilesAndNetwork/1/FileReading/src/main/java/Loader.java
+++ 09_FilesAndNetwork/1/FileReading/src/main/java/Loader.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -10,30 +11,20 @@ public class Loader {
     static long longFolderSize = 0L;
     public static void main(String[] args) {
         try {
-            Scanner console = new Scanner(System.in);
-            System.out.println("Введите путь до папки:");
-            String strPath = console.nextLine();
+            String strPath ;
+            if (args.length > 0) {
+                strPath = args[0];
+            } else {
+                Scanner console = new Scanner(System.in);
+                System.out.println("Введите путь до папки:");
+                strPath = console.nextLine();
+            }
             Path path = Paths.get(strPath);
             if (Files.exists(path) && Files.isDirectory(path)) {
                 getSizeOfFolder(path);
-                double folderSize;
-                String unit;
-                if (longFolderSize > Math.pow(1024,3)) {
-                    folderSize = longFolderSize / Math.pow(1024, 3);
-                    unit = "Гб";
-                }
-                else if (longFolderSize > Math.pow(1024,2)) {
-                    folderSize = longFolderSize / Math.pow(1024, 2);
-                    unit = "Мб";
-                }
-                else if (longFolderSize > Math.pow(1024,1)) {
-                    folderSize = longFolderSize / Math.pow(1024, 1);
-                    unit = "Кб";
-                } else {
-                    folderSize = longFolderSize;
-                    unit = "Б";
-                }
-                System.out.printf("Размер папки %1s составляет %2.1f%3s.%n", strPath, folderSize, unit);
+                HashMap <Double, String> folderSizeAndUnit  = getSizeAndUnit(longFolderSize);
+                System.out.printf("Размер папки %1s составляет %2.1f%3s.%n", strPath,
+                        folderSizeAndUnit.keySet().iterator().next(), folderSizeAndUnit.values().iterator().next());
             }
             else {
                 System.out.printf("Папки %s не существует.%n", strPath);
@@ -60,5 +51,21 @@ public class Loader {
                 }
             });
         }
+    }
+
+    public static HashMap<Double, String> getSizeAndUnit(Long longSize) {
+        HashMap<Double, String> sizeAndUnit = new HashMap<>();
+        if (longSize > Math.pow(1024,3)) {
+            sizeAndUnit.put ((longSize / Math.pow(1024, 3)),"Гб");
+        }
+        else if (longSize > Math.pow(1024,2)) {
+            sizeAndUnit.put ((longSize / Math.pow(1024, 2)),"Мб");
+        }
+        else if (longSize > Math.pow(1024,1)) {
+            sizeAndUnit.put ((longSize / Math.pow(1024, 1)),"Кб");
+        } else {
+            sizeAndUnit.put ((double) longSize ,"Б");
+        }
+        return sizeAndUnit;
     }
 }
